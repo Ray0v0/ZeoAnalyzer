91a8e23ee15d889284d180dd901195589ad31f81
Ray <947609598@qq.com>
2025-04-09 14:04:02 +0800

    move database directory out of this repo

--- src/main.py
+++ src/main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
         feature_extractors=feature_extractors,
         output_dir="./demo_output_1000"
     )
-    cif_dir = "../database/demo_dataset_1000"
+    cif_dir = "../../database/demo_dataset_1000"
     analyzer.analyse_cifs(cif_dir, zeopp_already_invoked=True)
 
